e4c02fcee748dbe688405632409c6f3f0ca091d4
Zhichao Jin <jzc109@users.noreply.github.com>
2023-08-16 15:52:20 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+
+st.set_page_config(
+    page_title="Introducion",
+)
+
+
+st.write("# This Web is for Medical Statistics Teaching")
+
+
+st.markdown(
+    """
+### Welcome!
+该系列网页利用Python结合Streamlit实现了《医学统计学》课程中若干基本概念的可视化，后续将继续加入一些基本概念的讲解，供本科教学使用。
+"""
+)
+
